4d0767a6995d05a3dce11e5b862a73e67323d9aa
Janek <jan.piotr.latko@gmail.com>
2021-11-13 19:58:07 +0100

    log metrics

--- scripts/image_nll.py
+++ scripts/image_nll.py
@@ -89,6 +89,8 @@ def run_bpd_evaluation(model, diffusion, data, num_samples, clip_denoised):
         wandb.log({"L_intermediate": np.mean(minibatch_metrics["vb"][:,:-1].sum(dim=1).detach().cpu().numpy())})
         wandb.log({"L_0": np.mean(minibatch_metrics["vb"][:,-1].detach().cpu().numpy())})
         wandb.log({"L_T": np.mean(minibatch_metrics["prior_bpd"].detach().cpu().numpy())})
+        wandb.log({"mse": np.mean(minibatch_metrics["mse"].detach().cpu().numpy())})
+        wandb.log({"xstart_mse": np.mean(minibatch_metrics["xstart_mse"].detach().cpu().numpy())})
 
         logger.log(f"done {num_complete} samples: bpd={np.mean(all_bpd)}")
 
